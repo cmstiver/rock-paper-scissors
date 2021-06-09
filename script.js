1d4748ed436004b0fae4e2eb81f998a786bacf2f
Chayse Stiver <cmstiver1@gmail.com>
2021-06-09 01:45:02 -0700

    Created function to randomly return computer's choice

---
+++ script.js
@@ -0,0 +1,10 @@
+//Randomly return rock, paper, or scissors.
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {return 'Rock'
+        } else if (choice === 1) {return 'Paper'
+            } else {
+                return 'Scissors'
+                }
+}
+console.log(computerPlay())
