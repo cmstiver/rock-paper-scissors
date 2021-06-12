4f0d0bc97f70cfcfa2a2a7689fa6e011eeb81c80
Chayse Stiver <cmstiver1@gmail.com>
2021-06-12 11:44:54 -0700

    Added UI for the game.

--- old-code.js
+++ old-code.js
@@ -1,5 +1,5 @@
 //Randomly return rock, paper, or scissors.
-function createChoice() {
+function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {return 'rock'
         } else if (choice === 1) {return 'paper'
@@ -7,7 +7,15 @@ function createChoice() {
                 return 'scissors'
                 };
 }
-function checkResult() {
+function playRound() {
+    let playerSelectionInitial = prompt('Rock, Paper, or Scissors?'); //player input
+    let playerSelection = playerSelectionInitial.toLowerCase();
+    console.log(playerSelection)
+    let computerSelection = computerPlay(); //computer input
+    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+        alert('Please enter rock, paper, or scissors.')
+        playRound()
+    } else {
         if (playerSelection === 'rock' && computerSelection === 'paper')
         {
             alert('Failure. The computer chose '+computerSelection+'.');
@@ -36,66 +44,46 @@ function checkResult() {
             alert('Tie. The computer chose '+computerSelection+'.');
         }
   }
-
-function inputAnswer() {
-    playerSelection = (this.id)
-    playRound(playerSelection, computerSelection)
 }
 
-function playRound() {
-    computerSelection = createChoice()
-    let result = checkResult()
-    if (result === 'win') {
-            pScore++
-            round++
-            pScoreCounter.textContent = 'Your Score: '+pScore
-            roundCounter.textContent = 'Round: '+round
-            checkScore()
-    } else if (result === 'fail') {
-            cScore++
-            round++
-            cScoreCounter.textContent = 'Your Score: '+cScore
-            roundCounter.textContent = 'Round: '+round
-            checkScore()
-     } else {
-            round++
-            roundCounter.textContent = 'Round: '+round
-            checkScore()
-        }
+function game(round) {
+       if (round === 'win') {
+           pScore = ++pScore
+       } else if (round === 'fail') {
+           cScore = ++cScore
+       } else {}
+    console.log('Player: '+pScore)
+    console.log('Computer: '+cScore)
 }
-function checkScore() {
+
+function checkScore(){
     if (pScore >= 3) {
         alert('You win, you super cool dude.')
     } else if (cScore >= 3) {
         alert('Uh-oh, you\'re a big loser.')
-    } else if (round === 5) {
-        if (pScore > cScore) {
-            alert('You win, you super cool dude.')
-        } else {
-            alert('Uh-oh, you\'re a big loser.')
-        }
+    } else {
+        game(playRound())
     }
-    }
+}
 
+let pScore = 0;
+let cScore = 0;
 
+alert('Round: 1 | Player: '+pScore +' | Computer: '+cScore)
+game(playRound())
+alert('Round: 2 | Player: '+pScore +' | Computer: '+cScore)
+checkScore() //I know there's a better way, but I don't know how yet lol.
+alert('Round: 3 | Player: '+pScore +' | Computer: '+cScore)
+checkScore()
+alert('Round: 4 | Player: '+pScore +' | Computer: '+cScore)
+checkScore()
+alert('Round: 5 | Player: '+pScore +' | Computer: '+cScore)
+checkScore()
 
-
-
-
-
-
-let playerSelection
-let computerSelection
-let pScore = 0
-let cScore = 0
-let round = 0
-let pScoreCounter = document.querySelector('#pScore')
-let cScoreCounter = document.querySelector('#cScore')
-let roundCounter = document.querySelector('#round')
-
-let rock = document.querySelector('#rock');
-rock.addEventListener('click', inputAnswer);
-let paper = document.querySelector('#paper');
-paper.addEventListener('click', inputAnswer)
-let scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', inputAnswer)
+if (pScore > cScore) {
+    alert('You win, you super cool dude.')
+} else if (pScore === cScore){
+    alert ('The computer is formidable. It\'s a draw.')
+} else {
+    alert('Uh-oh, you\'re a big loser.')
+}
